652fe55a95a6bd9137b6a25c1c03f54378bbc8a8
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-20 22:59:37 -0400

    Extract homepage skills to component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Skills extends Component {
+  render() {
+    return (
+      <div className="section">
+        <div className="container">
+          <h2>Skills</h2>
+          <div className="clients-logo-wrapper text-center row">
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/ruby.png" alt="ruby" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/rails.png" alt="rails" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/react.png" alt="react" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/redux.png" alt="redux" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/js.png" alt="js" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/jQuery.png" alt="jQuery" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/github.png" alt="github" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="skills.html"><img src="img/logos/color/heroku.png" alt="heroku" /></a></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Skills;
