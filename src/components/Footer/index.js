c40ee5d08db2060dd2436bd04e2e4eb42077c0be
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-20 22:56:00 -0400

    Extract homepage footer to component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-footer col-md-6 col-xs-6">
+              <h3>Navigate</h3>
+              <ul className="no-list-style footer-navigate-section">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="skills.html">Skills</a></li>
+                <li><a href="hire.html">Hire</a></li>
+              </ul>
+            </div>
+            <div className="col-footer col-md-4 col-xs-6">
+              <h3>Contacts</h3>
+              <p className="contact-us-details">
+                <b>Homebase:</b> Boston, Massachusetts<br/>
+                <b>Email:</b> <a href="mailto:andrew@andrewpalmer.co">andrew@andrewpalmer.co</a><br/>
+                <b>Github:</b> <a href="https://github.com/apalmer0/" target="_blank">github.com/apalmer0</a><br/>
+                <b>LinkedIn:</b> <a href="https://linkedin.com/in/apalmer0/" target="_blank">linkedin.com/in/apalmer0</a><br/>
+                <b>Phone:</b> 617.690.8147<br/>
+              </p>
+            </div>
+            <div className="col-footer col-md-2 col-xs-6">
+              <h3>Stay Connected</h3>
+              <ul className="footer-stay-connected no-list-style">
+                <li><a href="https://github.com/apalmer0" target="_blank" className="github"></a></li>
+                <li><a href="https://www.linkedin.com/in/apalmer0" target="_blank" className="linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="footer-copyright">
+                &copy; 2016 Andrew Palmer. All rights reserved.
+                <br />
+                Some icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
