b991e9e3ac2fb92c0ad3bdabe2100d4690d23b64
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-21 22:29:07 -0400

    Small updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,7 +19,6 @@ class Footer extends Component {
             <div className="col-footer col-md-4 col-xs-6">
               <h3>Contacts</h3>
               <p className="contact-us-details">
-                <b>Homebase:</b> Boston, Massachusetts<br/>
                 <b>Email:</b> <a href="mailto:andrew@andrewpalmer.co">andrew@andrewpalmer.co</a><br/>
                 <b>Github:</b> <a href="https://github.com/apalmer0/" target="_blank">github.com/apalmer0</a><br/>
                 <b>LinkedIn:</b> <a href="https://linkedin.com/in/apalmer0/" target="_blank">linkedin.com/in/apalmer0</a><br/>
