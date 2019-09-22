ac5b91e8064b295b72e46f2b396212464c312ad6
Andrew Palmer <apalmer0@users.noreply.github.com>
2019-09-21 22:30:35 -0400

    Small updates (#1)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/ce0758df-f820-4051-a785-b59dcc905fca/deploy-status)](https://app.netlify.com/sites/nifty-knuth-c1f72b/deploys)
+
 # Andrew Palmer portfolio site
 
 Built using the **mPurpose** theme template.
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,23 +3,32 @@ import { Link } from 'react-router-dom';
 
 class Skills extends Component {
   render() {
+    const data = [
+      { skill: "ruby", logo: "img/logos/color/ruby.png" },
+      { skill: "rails", logo: "img/logos/color/rails.png" },
+      { skill: "react", logo: "img/logos/color/react.png" },
+      { skill: "redux", logo: "img/logos/color/redux.png" },
+      { skill: "js", logo: "img/logos/color/js.png" },
+      { skill: "jQuery", logo: "img/logos/color/jQuery.png" },
+      { skill: "github", logo: "img/logos/color/github.png" },
+      { skill: "heroku", logo: "img/logos/color/heroku.png" },
+    ];
+
     return (
       <div className="section">
         <div className="container">
           <h2>Skills</h2>
           <div className="clients-logo-wrapper text-center row">
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/ruby.png" alt="ruby" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/rails.png" alt="rails" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/react.png" alt="react" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/redux.png" alt="redux" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/js.png" alt="js" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/jQuery.png" alt="jQuery" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/github.png" alt="github" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"><Link to="/skills"><img src="img/logos/color/heroku.png" alt="heroku" /></Link></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
-            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6"></div>
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6" />
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6" />
+            {data.map(({ skill, logo }) => (
+                <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6">
+                  <Link to="/skills"><img src={logo} alt={skill} /></Link>
+                </div>
+              )
+            )}
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6" />
+            <div className="col-lg-1 col-md-1 col-sm-3 col-xs-6" />
           </div>
         </div>
       </div>
