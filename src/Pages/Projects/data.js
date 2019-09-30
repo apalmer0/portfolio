c6077e418f0bef5ad707c7f4ca0eb2bd24bd9eb2
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-29 21:43:11 -0400

    Create ProjectItem component and Projects page to render them

---
+++ src/Pages/Projects/data.js
@@ -0,0 +1,68 @@
+const projectData = [
+  {
+    title: "Magic Mirror",
+    category: "Development",
+    image: "img/headshot.png",
+    description: "I built a mirror that does stuff",
+    slug: "magic-mirror",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Weather Records",
+    category: "Development",
+    image: "img/headshot.png",
+    description: "Today's hot! But is it hot relative to history?",
+    slug: "weather-records",
+    body: "this is the content of the article",
+  },
+  {
+    title: "The Fly",
+    category: "Climbing",
+    image: "img/headshot.png",
+    description: "I had this goal for like 20 years and then I did it",
+    slug: "the-fly",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Brother's Keeper",
+    category: "Climbing",
+    image: "img/headshot.png",
+    description: "I had this goal for like 2 years and I'm still working on it",
+    slug: "brothers-keeper",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Ring Return",
+    category: "Tinkering",
+    image: "img/hook.png",
+    description: "I have ",
+    slug: "ring-return",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Mac Stand",
+    category: "Tinkering",
+    image: "img/headshot.png",
+    description: "I didn't want to pay $35 for a metal stand",
+    slug: "mac-stand",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Jaws II",
+    category: "Climbing",
+    image: "img/headshot.png",
+    description: "Pulling off what I never thought I'd be capable of",
+    slug: "jaws-ii",
+    body: "this is the content of the article",
+  },
+  {
+    title: "Confident Man Sit",
+    category: "Climbing",
+    image: "img/headshot.png",
+    description: "A first ascent in a heavily-traveled area",
+    slug: "confident-man-sit",
+    body: "this is the content of the article",
+  },
+];
+
+export default projectData;
---
+++ src/components/ProjectItem/styles.js
@@ -0,0 +1,34 @@
+export default {
+  projectItem: {
+    borderBottom: '1px solid #105BA1',
+    cursor: 'pointer',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingBottom: '50px',
+    paddingTop: '50px',
+  },
+  imageContainer: {
+    display: 'inline',
+    marginRight: '15px',
+  },
+  image: {
+    maxWidth: '200px',
+    maxHeight: '200px',
+    verticalAlign: 'top',
+  },
+  contentContainer: {
+    display: 'inline-block',
+    marginLeft: '15px',
+    width: '65%',
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: '600',
+  },
+  category: {
+    fontSize: '10px',
+  },
+  description: {
+    fontSize: '15px',
+  },
+}
