fac27f15008ce438a22b008d2bf46c870c91745c
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-29 21:45:30 -0400

    Overhaul Home page content and style

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
-import Skills from '../../components/Skills'
+import Hero from '../../components/Hero'
+import styles from './styles';
 
 class Home extends Component {
   render() {
@@ -11,88 +12,43 @@ class Home extends Component {
       <div>
         <Header activeTab="home" />
 
-        <div className="homepage-slider">
-          <div id="sequence">
-            <ul className="sequence-canvas">
-              <li className="bg1">
-                <h2 className="title">Hi, I'm Andrew.</h2>
-                <h3 className="subtitle">A professional, full-stack web developer with a strong analytical and client-facing background.</h3>
-              </li>
-              <li className="bg2">
-                <h2 className="title">Hi, I'm Andrew.</h2>
-                <h3 className="subtitle">A professional, full-stack web developer with a strong analytical and client-facing background.</h3>
-              </li>
-              <li className="bg3">
-                <h2 className="title">Hi, I'm Andrew.</h2>
-                <h3 className="subtitle">A professional, full-stack web developer with a strong analytical and client-facing background.</h3>
-              </li>
-            </ul>
-            <div className="sequence-pagination-wrapper">
-              <ul className="sequence-pagination">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-              </ul>
+        <Hero
+          image="/img/headshot.png"
+          name="Andrew Palmer"
+          description="Builder, Climber, Tinkerer"
+        >
+          Software Developer for <a href="https://thegnar.co" target="_blank">The Gnar Company</a>
+        </Hero>
+
+        <div style={styles.aboutContainer}>
+          <div style={styles.about}>
+            <div style={styles.aboutTitle}>About Me</div>
+            <div style={styles.aboutContent}>
+              I love writing code, solving problems, rock climbing, and building things. I'm happiest when I'm taking on interesting or challenging <Link to="/projects">Projects</Link>, and sometimes I <Link to="/writing">write</Link> about them.
+            </div>
+            <div style={styles.aboutContent}>
+              I'm a full-stack developer. My favorite side to work on is whichever side I'm currently working on.
+            </div>
+            <div style={styles.aboutContent}>
+              I spend most of my development time in Ruby on Rails, React, and Redux.
+            </div>
+            <div style={styles.aboutContent}>
+              I prefer slightly overhanging boulders with small holds, and I will do whatever I can physically manage to keep my feet on the wall.
+            </div>
+            <div style={styles.aboutContent}>
+              I'm currently working for The Gnar Company, a web and mobile software development consultancy in Boston, MA.
+            </div>
+          </div>
+          <div style={styles.about}>
+            <div style={styles.aboutTitle}>Get in Touch</div>
+            <div style={styles.contactOptions}>
+              <span className="icon-github">Github</span>
+              <span>LinkedIn</span>
+              <span>Email</span>
+              <span>Phone</span>
             </div>
           </div>
         </div>
-
-        <div className="section">
-          <div className="container">
-            <h2>Get in touch</h2>
-            <div className="row">
-              <div className="col-md-6 col-sm-12 col-xs-12">
-                <div className="service-wrapper">
-                  <img src="img/service-icon/climb.png" alt="Climbing" />
-                  <h3>Climbing</h3>
-                  <p>Rock climbing has taught me how to work hard and solve problems, skills I use as a developer.</p>
-                </div>
-              </div>
-              <div className="col-md-6 col-sm-12 col-xs-12">
-                <div className="service-wrapper">
-                  <img src="img/service-icon/burrito.png" alt="Motivation" />
-                  <h3>Motivation</h3>
-                  <p>Rewards are best when they're earned. In climbing and in life, you have to earn your burrito.</p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 col-sm-6">
-                <a href="https://github.com/apalmer0" target="_blank">
-                  <div className="service-wrapper portfolio-item">
-                    <img src="img/service-icon/github.png" alt="Github" />
-                    <h3>Github</h3>
-                  </div>
-                </a>
-              </div>
-              <div className="col-md-6 col-sm-6">
-                <Link to="/hire">
-                  <div className="service-wrapper portfolio-item">
-                    <img src="img/service-icon/hire.png" alt="Hire" />
-                    <h3>Hire</h3>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="section section-white">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="calltoaction-wrapper">
-                  <h3>Get in touch today!</h3>
-                  <div>
-                    <Link to="hire" className="btn btn-orange">Contact</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <Skills />
 
         <Footer />
       </div>
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,25 @@
+export default {
+  aboutContainer: {
+    fontFamily: 'Be Vietnam',
+    margin: '60px auto',
+    width: '40%',
+  },
+  about: {
+    marginBottom: '60px',
+  },
+  aboutTitle: {
+    fontSize: '25px',
+    marginBottom: '20px',
+    fontWeight: '600',
+  },
+  aboutContent: {
+    fontSize: '18px',
+    lineHeight: '1.5',
+    marginBottom: '30px',
+  },
+  contactOptions: {
+    display: 'flex',
+    marginTop: '30px',
+    justifyContent: 'space-evenly',
+  },
+};
