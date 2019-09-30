2ceafe7dabd46037fb9b6a435ed8b5a1b3e562eb
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-09-29 21:40:49 -0400

    Absolute path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,7 +17,7 @@ class Header extends Component {
           <nav id="mainmenu" className="mainmenu navbar navbar-default">
             <div className="navbar-header">
               <Link to="/" className="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></Link>
-              <Link to="/"><img className="logo-wrapper" src="img/ap_logo.png" alt="Andrew Palmer portfolio" /></Link>
+              <Link to="/"><img className="logo-wrapper" src="/img/ap_logo.png" alt="Andrew Palmer portfolio" /></Link>
             </div>
             <div id="navbar" className="navbar-collapse collapse">
               <ul className="nav navbar-nav navbar-right">
